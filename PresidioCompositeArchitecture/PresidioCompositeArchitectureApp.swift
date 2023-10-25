c189725d823ade7b2e836fd4adff52d14b4cd84b
immanuel nowpert <nowpertimmanuel@gmail.com>
2023-10-25 10:22:19 +0530

    Initial Commit

---
+++ PresidioCompositeArchitecture/PresidioCompositeArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresidioCompositeArchitectureApp.swift
+//  PresidioCompositeArchitecture
+//
+//  Created by immanuel nowpert on 25/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PresidioCompositeArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
